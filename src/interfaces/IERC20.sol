1287e935d6d1f0138accb8343898a83e62ebd8cf
Sambhav Dusad <sambhavdusad24@gmail.com>
2022-03-13 15:56:16 +0530

    feat: ⚡️ init

---
+++ src/interfaces/IERC20.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.10;
+
+interface IERC20 {
+	function balanceOf(address) external view returns (uint256);
+	function transfer(address to, uint256 amount) external;
+}
