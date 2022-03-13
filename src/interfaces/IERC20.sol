d73b4949181dce91423d203d4a49a4f8a9b772d3
Sambhav Dusad <sambhavdusad24@gmail.com>
2022-03-14 00:19:49 +0530

    chore: 🚤 add husky

--- src/interfaces/IERC20.sol
+++ src/interfaces/IERC20.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >= 0.8.10;
+pragma solidity >=0.8.10;
 
 interface IERC20 {
-	function balanceOf(address) external view returns (uint256);
-	function transfer(address to, uint256 amount) external;
+    function balanceOf(address) external view returns (uint256);
+
+    function transfer(address to, uint256 amount) external returns (bool);
 }
