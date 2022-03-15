85aa5266b2e3458cef8cb3cdd7cff8b697b93ba6
Sambhav Dusad <sambhavdusad24@gmail.com>
2022-03-15 14:28:03 +0530

    feat: ⚔️ complete core contracts and tests

---
+++ src/UnifapV2Pair.sol
@@ -0,0 +1,186 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10;
+
+import "solmate/tokens/ERC20.sol";
+import "solmate/utils/FixedPointMathLib.sol";
+import "solmate/utils/ReentrancyGuard.sol";
+import "./interfaces/IERC20.sol";
+import "./libraries/Math.sol";
+
+error InsufficientLiquidityMinted();
+error InsufficientLiquidityBurned();
+error SafeTransferFailed();
+error SwapToSelf();
+error InsufficientLiquidity();
+error InvalidAmount();
+error InvalidConstantProductFormula();
+
+/// @title UnifapV2Pair
+/// @author Uniswap Labs
+/// @notice maintains a liquidity pool of a pair of tokens
+contract UnifapV2Pair is ERC20, ReentrancyGuard {
+    // ========= Libraries =========
+    using Math for uint256;
+    using FixedPointMathLib for uint256;
+
+    // ========= Constants =========
+    uint256 public constant MINIMUM_LIQUIDITY = 1e3;
+    bytes4 public constant SELECTOR =
+        bytes4(keccak256("transfer(address,uint256"));
+
+    // ========= State Variables =========
+    address public token0;
+    address public token1;
+
+    uint112 public reserve0;
+    uint112 public reserve1;
+
+    // ======== Events ========
+    event Mint(address indexed _operator, uint256 _value);
+    event Burn(address indexed _operator, uint256 _value);
+    event Sync(uint112 reserve0, uint112 reserve1);
+    event Swap(
+        address sender,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to
+    );
+
+    // ========= Constructor =========
+    constructor(address _token0, address _token1)
+        ERC20("UnizwapV2", "UNI-V2", 18)
+    {
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+    function getReserves()
+        public
+        view
+        returns (
+            uint112,
+            uint112,
+            bytes32
+        )
+    {
+        return (reserve0, reserve1, 0);
+    }
+
+    /// @notice calculate the pool tokens for the given new liquidity amount
+    /// @dev if new pool is created, then minimum liquidity is 1e3 transfered to 0x0
+    /// @param to address to which pool tokens are minted
+    /// @return liquidity total liquidity minted
+    function mint(address to) public nonReentrant returns (uint256 liquidity) {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        uint256 amount0 = balance0 - uint256(reserve0);
+        uint256 amount1 = balance1 - uint256(reserve1);
+
+        if (totalSupply == 0) {
+            // Initial liquidity = sqrt(a0 * a1)
+            liquidity =
+                FixedPointMathLib.sqrt(amount0 * amount1) -
+                MINIMUM_LIQUIDITY;
+
+            // Prevents value of 1 LP token being too high
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            // Minimum because max is prone to price manipulation
+            liquidity = Math.min(
+                (amount0 * totalSupply) / reserve0,
+                (amount1 * totalSupply) / reserve1
+            );
+        }
+
+        if (liquidity == 0) revert InsufficientLiquidityMinted();
+
+        _mint(to, liquidity);
+
+        _update(balance0, balance1);
+
+        emit Mint(to, liquidity);
+    }
+
+    /// @notice burns pool tokens of a particular address
+    /// @dev needs to transfer pool tokens to the pool first to be burnt
+    /// @param to address whose tokens are burned
+    /// @return amount0 amount of token0 burned
+    /// @return amount1 amount of token1 burned
+    function burn(address to)
+        public
+        nonReentrant
+        returns (uint256 amount0, uint256 amount1)
+    {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        uint256 liquidity = balanceOf[address(this)];
+
+        amount0 = (liquidity * balance0) / uint256(reserve0);
+        amount1 = (liquidity * balance1) / uint256(reserve1);
+
+        if (amount0 == 0 || amount1 == 0) revert InsufficientLiquidityBurned();
+
+        _burn(address(this), liquidity);
+
+        _safeTransfer(token0, to, amount0);
+        _safeTransfer(token1, to, amount1);
+
+        _update(balance0 - amount0, balance1 - amount1);
+
+        emit Burn(to, liquidity);
+    }
+
+    /// @notice swaps two tokens
+    /// @dev new balances should maintain the constant product formula
+    /// @param amount0Out amount of token0 to be transfered
+    /// @param amount1Out amount of token1 to be transfered
+    /// @param to address to which tokens are transfered
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to
+    ) public nonReentrant {
+        if (amount0Out == 0 && amount1Out == 0) revert InvalidAmount();
+
+        if (amount0Out > reserve0 || amount1Out > reserve1)
+            revert InsufficientLiquidity();
+
+        if (to == token0 || to == token1) revert SwapToSelf();
+
+        if (amount0Out != 0) _safeTransfer(token0, to, amount0Out);
+        if (amount1Out != 0) _safeTransfer(token1, to, amount1Out);
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        if (balance0 * balance1 < uint256(reserve0) * uint256(reserve1))
+            revert InvalidConstantProductFormula();
+
+        _update(balance0, balance1);
+
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
+    }
+
+    // ========= Internal functions =========
+
+    /// @notice updates pool reserves
+    /// @param balance0 new balance of token0 in pool
+    /// @param balance1 new balance of token1 in pool
+    function _update(uint256 balance0, uint256 balance1) internal {
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+
+        emit Sync(reserve0, reserve1);
+    }
+
+    function _safeTransfer(
+        address token,
+        address to,
+        uint256 amount
+    ) internal {
+        bool success = IERC20(token).transfer(to, amount);
+        if (!success) revert SafeTransferFailed();
+    }
+}
---
+++ src/test/UnifapV2Pair.t.sol
@@ -0,0 +1,319 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10;
+
+import {DSTest} from "ds-test/test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {stdError} from "forge-std/stdlib.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+import {UnifapV2Pair} from "../UnifapV2Pair.sol";
+
+contract MockUser {
+    function addLiquidity(
+        address pair,
+        address _token0,
+        address _token1,
+        uint256 _amount0,
+        uint256 _amount1
+    ) public returns (uint256) {
+        ERC20(_token0).transfer(pair, _amount0);
+        ERC20(_token1).transfer(pair, _amount1);
+
+        return UnifapV2Pair(pair).mint(address(this));
+    }
+
+    function removeLiquidity(address pair, uint256 liquidity)
+        public
+        returns (uint256, uint256)
+    {
+        UnifapV2Pair(pair).transfer(pair, liquidity);
+
+        return UnifapV2Pair(pair).burn(address(this));
+    }
+}
+
+contract TestUnifapV2Pair is DSTest {
+    Vm public constant vm = Vm(HEVM_ADDRESS);
+
+    MockERC20 public token0;
+    MockERC20 public token1;
+    UnifapV2Pair public pair;
+    MockUser public user;
+
+    function setUp() public {
+        token0 = new MockERC20("UnifapToken0", "UT0", 18);
+        token1 = new MockERC20("UnifapToken1", "UT1", 18);
+        pair = new UnifapV2Pair(address(token0), address(token1));
+
+        token0.mint(address(this), 10 ether);
+        token1.mint(address(this), 10 ether);
+
+        user = new MockUser();
+        token0.mint(address(user), 10 ether);
+        token1.mint(address(user), 10 ether);
+    }
+
+    function assertPairReserves(uint256 _reserve0, uint256 _reserve1) public {
+        assertEq(pair.reserve0(), _reserve0);
+        assertEq(pair.reserve1(), _reserve1);
+    }
+
+    function testMintNewPair() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        uint256 liquidity = pair.mint(address(this));
+
+        assertPairReserves(1 ether, 1 ether);
+        assertEq(pair.balanceOf(address(0)), pair.MINIMUM_LIQUIDITY());
+        assertEq(pair.balanceOf(address(this)), liquidity);
+        assertEq(pair.totalSupply(), liquidity + pair.MINIMUM_LIQUIDITY());
+    }
+
+    function testMintWithReserve() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        uint256 l1 = pair.mint(address(this));
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 2 ether);
+        uint256 l2 = pair.mint(address(this));
+
+        assertPairReserves(3 ether, 3 ether);
+        assertEq(pair.balanceOf(address(this)), l1 + l2);
+        assertEq(pair.totalSupply(), l1 + l2 + pair.MINIMUM_LIQUIDITY());
+    }
+
+    function testMintUnequalBalance() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        uint256 l1 = pair.mint(address(this));
+
+        token0.transfer(address(pair), 4 ether);
+        token1.transfer(address(pair), 1 ether);
+        uint256 l2 = pair.mint(address(this));
+
+        assertPairReserves(5 ether, 2 ether);
+        assertEq(pair.balanceOf(address(this)), l1 + l2);
+        assertEq(pair.totalSupply(), l1 + l2 + pair.MINIMUM_LIQUIDITY());
+    }
+
+    function testMintArithmeticUnderflow() public {
+        // 0x11: Arithmetic over/underflow
+        vm.expectRevert(stdError.arithmeticError);
+
+        pair.mint(address(this));
+    }
+
+    function testMintInsufficientLiquidity() public {
+        token0.transfer(address(pair), 1000);
+        token1.transfer(address(pair), 1000);
+
+        vm.expectRevert(
+            abi.encodeWithSignature("InsufficientLiquidityMinted()")
+        );
+        pair.mint(address(this));
+    }
+
+    function testMintMultipleUsers() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        uint256 l1 = pair.mint(address(this));
+
+        uint256 l2 = user.addLiquidity(
+            address(pair),
+            address(token0),
+            address(token1),
+            2 ether,
+            3 ether
+        );
+
+        assertPairReserves(3 ether, 4 ether);
+        assertEq(pair.balanceOf(address(this)), l1);
+        assertEq(pair.balanceOf(address(user)), l2);
+        assertEq(pair.totalSupply(), l1 + l2 + pair.MINIMUM_LIQUIDITY());
+    }
+
+    function testBurn() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        uint256 liquidity = pair.mint(address(this));
+
+        pair.transfer(address(pair), liquidity);
+        pair.burn(address(this));
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertEq(pair.totalSupply(), pair.MINIMUM_LIQUIDITY());
+        assertPairReserves(pair.MINIMUM_LIQUIDITY(), pair.MINIMUM_LIQUIDITY());
+        assertEq(
+            token0.balanceOf(address(this)),
+            10 ether - pair.MINIMUM_LIQUIDITY()
+        );
+        assertEq(
+            token1.balanceOf(address(this)),
+            10 ether - pair.MINIMUM_LIQUIDITY()
+        );
+    }
+
+    function testBurnUnequal() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        uint256 l0 = pair.mint(address(this));
+
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 2 ether);
+        uint256 l1 = pair.mint(address(this));
+
+        pair.transfer(address(pair), l0 + l1);
+        (uint256 amount0, uint256 amount1) = pair.burn(address(this));
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertEq(pair.totalSupply(), pair.MINIMUM_LIQUIDITY());
+        assertEq(token0.balanceOf(address(this)), 10 ether - 2 ether + amount0);
+        assertEq(token1.balanceOf(address(this)), 10 ether - 3 ether + amount1);
+    }
+
+    function testBurnNoLiquidity() public {
+        // 0x12: divide/modulo by zero
+        vm.expectRevert(stdError.divisionError);
+
+        pair.burn(address(this));
+    }
+
+    function testBurnInsufficientLiquidityBurned() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+
+        vm.expectRevert(
+            abi.encodeWithSignature("InsufficientLiquidityBurned()")
+        );
+        pair.burn(address(this));
+    }
+
+    function testBurnMultipleUsers() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        uint256 l1 = pair.mint(address(this));
+
+        uint256 l2 = user.addLiquidity(
+            address(pair),
+            address(token0),
+            address(token1),
+            2 ether,
+            3 ether
+        );
+
+        pair.transfer(address(pair), l1);
+        (uint256 amount0, uint256 amount1) = pair.burn(address(this));
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertEq(pair.balanceOf(address(user)), l2);
+        assertEq(pair.totalSupply(), l2 + pair.MINIMUM_LIQUIDITY());
+        assertPairReserves(3 ether - amount0, 4 ether - amount1);
+        assertEq(token0.balanceOf(address(this)), 10 ether - 1 ether + amount0);
+        assertEq(token1.balanceOf(address(this)), 10 ether - 1 ether + amount1);
+    }
+
+    function testBurnUnbalancedMultipleUsers() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        uint256 l1 = pair.mint(address(this));
+
+        uint256 l2 = user.addLiquidity(
+            address(pair),
+            address(token0),
+            address(token1),
+            2 ether,
+            3 ether
+        );
+
+        (uint256 a00, uint256 a01) = user.removeLiquidity(address(pair), l2);
+
+        pair.transfer(address(pair), l1);
+        (uint256 a10, uint256 a11) = pair.burn(address(this));
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertEq(pair.balanceOf(address(user)), 0);
+        assertEq(pair.totalSupply(), pair.MINIMUM_LIQUIDITY());
+
+        // second user penalised for unbalanced liquidity, hence reserves unbalanced
+        assertPairReserves(
+            pair.MINIMUM_LIQUIDITY(),
+            1 ether + pair.MINIMUM_LIQUIDITY()
+        );
+        assertEq(token0.balanceOf(address(this)), 10 ether - 1 ether + a10);
+        assertEq(token1.balanceOf(address(this)), 10 ether - 1 ether + a11);
+        assertEq(token0.balanceOf(address(user)), 10 ether - 2 ether + a00);
+        assertEq(token1.balanceOf(address(user)), 10 ether - 3 ether + a01);
+    }
+
+    function testSwap() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+
+        // transfer to maintain K
+        token1.transfer(address(pair), 1 ether);
+
+        pair.swap(0.5 ether, 0 ether, address(user));
+
+        assertPairReserves(0.5 ether, 2 ether);
+        assertEq(token0.balanceOf(address(user)), 10 ether + 0.5 ether);
+    }
+
+    function testSwapMultipleUserLiquidity() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+
+        user.addLiquidity(
+            address(pair),
+            address(token0),
+            address(token1),
+            2 ether,
+            3 ether
+        );
+
+        // transfer to maintain K
+        token0.transfer(address(pair), 1 ether);
+
+        pair.swap(0 ether, 1 ether, address(user));
+
+        assertPairReserves(4 ether, 3 ether);
+    }
+
+    function testSwapInvalidAmount() public {
+        vm.expectRevert(abi.encodeWithSignature("InvalidAmount()"));
+        pair.swap(0 ether, 0 ether, address(user));
+    }
+
+    function testSwapInsufficientLiquidity() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+
+        vm.expectRevert(abi.encodeWithSignature("InsufficientLiquidity()"));
+        pair.swap(3 ether, 0 ether, address(user));
+    }
+
+    function testSwapSwapToSelf() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+
+        vm.expectRevert(abi.encodeWithSignature("SwapToSelf()"));
+        pair.swap(1 ether, 0 ether, address(token0));
+    }
+
+    function testSwapInvalidConstantProductFormula() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+
+        vm.expectRevert(
+            abi.encodeWithSignature("InvalidConstantProductFormula()")
+        );
+        pair.swap(1 ether, 0 ether, address(user));
+    }
+}
